e7268a2c040f5a61e26e5f595209260e73dacc38
clarefausty <clarefausty@gmail.com>
2023-11-09 20:57:57 +0100

    imported the components in the App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
+
 import './App.css';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+import PlaceDetails from './components/PlaceDetails/PlaceDetails';
 
 function App() {
   return (
     <div className="App">
-      <p>Tomorrow is my birthday</p>
+      <Header/>
+      <List/>
+      <PlaceDetails/>
+      <Map/>
     </div>
   );
 }
