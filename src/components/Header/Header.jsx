e51c7e2d80160309096f6e63e8d0ee74fc0a8b64
clarefausty <clarefausty@gmail.com>
2023-11-09 23:48:36 +0100

    wip

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,26 @@ import { Autocomplete } from '@react-google-maps/api'
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core'
 
 const Header = () => {
-  return (
-    <AppBar>
-        <Toolbar className={classes.toolbar}>
+    return (
+        <AppBar>
+            <Toolbar className={classes.toolbar}>
+                <Typography variant='h5' className={classes.title}>
+                    Travel Aid
+                </Typography>
+                <Box display="flex">
+                    <Typography variant='h6' className={classes.title}>
+                        Explore New Places
+                    </Typography>
+                    <Autocomplete>
+                        <div className={classes.search}>
 
-        </Toolbar>
-    </AppBar>
-  )
+                        </div>
+                    </Autocomplete>
+                </Box>
+
+            </Toolbar>
+        </AppBar>
+    )
 }
 
 export default Header
