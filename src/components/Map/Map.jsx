7fae81092ad17e56c540ce263f40baf894354fae
clarefausty <clarefausty@gmail.com>
2023-11-10 10:45:15 +0100

    wip

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import googleMapReact from 'google-map-react'
+import { Paper, Topography, useMediaQuery } from '@material-ui/core'
+import LocationOutlinedIcon from "@material-ui/icons"
 const Map = () => {
   return (
     <div>Map</div>
