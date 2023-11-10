d0e054c1a8569b5e9b6986edf2ee18ffaa651fd6
clarefausty <clarefausty@gmail.com>
2023-11-10 11:22:47 +0100

    wip

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import googleMapReact from 'google-map-react'
 import { Paper, Topography, useMediaQuery } from '@material-ui/core'
-import LocationOutlinedIcon from "@material-ui/icons"
+import LocationOutlinedIcon from "@material-ui/icons/LocationOnOutlined"
+import { Rating } from '@material-ui/lab'
+import useStyles from "./styles"
 const Map = () => {
+    const classes = useStyles()
+    const isMobile = useMediaQuery('(min-width:600px)')
   return (
     <div>Map</div>
   )
